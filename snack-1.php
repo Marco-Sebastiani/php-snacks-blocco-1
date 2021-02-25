b0ef9643ec626edc66cce5929da0142464e59da3
Marco.seb <marco.seba96@gmail.com>
2021-02-25 16:01:29 +0100

    upgrade add array matches

--- snack-1.php
+++ snack-1.php
@@ -4,7 +4,53 @@ Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55 - 60 -->
 
 <?php 
+$matches = [
+    [
+        'homeTeam' =>'Acqua S.Bernardo Cantù',
+        'pointsHT' => '101',
+        'visitingTeam' =>'Allianz Pallacanestro Trieste',
+        'pointsVT' => '96'
+    ],
+    [
+        'homeTeam' =>'AIX Armani Exchange Milano',
+        'pointsHT' => '110',
+        'visitingTeam' =>'Banco di Sardegna Sassari',
+        'pointsVT' => '89'
+        
+    ],
+    [
+        'homeTeam' =>'Carpegna Prosciutto Pesaro',
+        'pointsHT' => '85',
+        'visitingTeam' =>'De\'Longhi Treviso',
+        'pointsVT' => '91'
+    ],
+    [
+        'homeTeam' =>'Dolomiti Energia Trentino',
+        'pointsHT' => '102',
+        'visitingTeam' =>'Fortitudo Lavoropiù Bologna',
+        'pointsVT' => '120'
+    ],
+    [
+        'homeTeam' =>'Germani Brescia',
+        'pointsHT' => '85',
+        'visitingTeam' =>'Happy Casa Brindisi',
+        'pointsVT' => '95'
+    ],
+    [
+        'homeTeam' =>'Openjobmetis Varese',
+        'pointsHT' => '81',
+        'visitingTeam' =>'Umana Reyer Venezia',
+        'pointsVT' => '65'
+    ],
+    [
+        'homeTeam' =>'UNAHOTELS Reggio Emilia',
+        'pointsHT' => '97',
+        'visitingTeam' =>'Vanoli Basket Cremona',
+        'pointsVT' => '98'
+    ]
+];
 
+var_dump($matches);
 ?>
 
 <!DOCTYPE html>
