dd6e9b579b877e770037f0389b263c45d7a519a3
Marco.seb <marco.seba96@gmail.com>
2021-02-25 16:23:20 +0100

    Git-finish snack-php-1

--- snack-1.php
+++ snack-1.php
@@ -7,50 +7,50 @@ Olimpia Milano - Cantù | 55 - 60 -->
 $matches = [
     [
         'homeTeam' =>'Acqua S.Bernardo Cantù',
-        'pointsHT' => '101',
+        'pointsHomeTeam' => '101',
         'visitingTeam' =>'Allianz Pallacanestro Trieste',
         'pointsVT' => '96'
     ],
     [
         'homeTeam' =>'AIX Armani Exchange Milano',
-        'pointsHT' => '110',
+        'pointsHomeTeam' => '110',
         'visitingTeam' =>'Banco di Sardegna Sassari',
         'pointsVT' => '89'
         
     ],
     [
         'homeTeam' =>'Carpegna Prosciutto Pesaro',
-        'pointsHT' => '85',
+        'pointsHomeTeam' => '85',
         'visitingTeam' =>'De\'Longhi Treviso',
         'pointsVT' => '91'
     ],
     [
         'homeTeam' =>'Dolomiti Energia Trentino',
-        'pointsHT' => '102',
+        'pointsHomeTeam' => '102',
         'visitingTeam' =>'Fortitudo Lavoropiù Bologna',
         'pointsVT' => '120'
     ],
     [
         'homeTeam' =>'Germani Brescia',
-        'pointsHT' => '85',
+        'pointsHomeTeam' => '85',
         'visitingTeam' =>'Happy Casa Brindisi',
         'pointsVT' => '95'
     ],
     [
         'homeTeam' =>'Openjobmetis Varese',
-        'pointsHT' => '81',
+        'pointsHomeTeam' => '81',
         'visitingTeam' =>'Umana Reyer Venezia',
         'pointsVT' => '65'
     ],
     [
         'homeTeam' =>'UNAHOTELS Reggio Emilia',
-        'pointsHT' => '97',
+        'pointsHomeTeam' => '97',
         'visitingTeam' =>'Vanoli Basket Cremona',
         'pointsVT' => '98'
     ]
 ];
 
-var_dump($matches);
+
 ?>
 
 <!DOCTYPE html>
@@ -59,9 +59,22 @@ var_dump($matches);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP snack 1; PHP snack 2</title>
 </head>
 <body>
-    
+    <h1>25-02-21 SECONDA GIORNATA</h1>
+
+    <p>
+        <?php for ($i = 0; $i < count($matches); $i++){
+
+            echo '<strong>SQUADRE:</strong> ' .$matches[$i]['homeTeam'].' - '.$matches[$i]['visitingTeam'].' | <strong>PUNTI:</strong> '.$matches[$i]['pointsHomeTeam'].' - '.$matches[$i]['pointsVT'] .'<br>';
+
+
+        }
+
+        ?></p>
+
+<!-- count ( Countable | array $value , int $mode=COUNT_NORMAL ): int
+Conta tutti gli elementi in un array o qualcosa in un oggetto. -->
 </body>
 </html>
